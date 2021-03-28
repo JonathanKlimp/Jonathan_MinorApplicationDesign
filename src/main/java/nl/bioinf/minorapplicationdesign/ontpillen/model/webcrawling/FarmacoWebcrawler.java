0d43735900770efcfb0d8d703986adea5f93eb65
larissa bouwknegt <l.bouwknegt@st.hanze.nl>
2021-03-28 13:42:41 +0200

    commit to switch devices

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/FarmacoWebcrawler.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/FarmacoWebcrawler.java
@@ -32,16 +32,22 @@ public class FarmacoWebcrawler extends AbstractWebcrawler {
         return null;
     }
 
+    private static Document getConnection(String medicine) throws IOException {
+        String basicUrl = "https://www.farmacotherapeutischkompas.nl/bladeren/preparaatteksten/";
+        String completeUrl = (basicUrl + medicine.charAt(0) + "/" + medicine).toLowerCase(Locale.ROOT);
+        Document doc = Jsoup.connect(completeUrl).get();
+        return doc;
+    }
+
     public static void main(String[] args) throws IOException {
         SSLHelper.bypassSSL();
-        List<String> list = List.of("Temazepam", "Citalopram", "Lorazepam");
-        String basicUrl = "https://www.farmacotherapeutischkompas.nl/bladeren/preparaatteksten/";
-        for (String medicine: list) {
-            String completeUrl = (basicUrl + medicine.charAt(0) + "/" + medicine).toLowerCase(Locale.ROOT);
-            System.out.println(completeUrl);
-            Document doc = Jsoup.connect(completeUrl).get();
-            Elements h2Tags = doc.getElementsByTag("h2").select(":contains(Bijwerkingen)");
-            System.out.println(h2Tags.nextAll().select("p").eachText());
+        List<String> druglist = List.of("Temazepam", "Citalopram", "Lorazepam");
+
+        for (String medicine: druglist) {
+            Document doc = getConnection(medicine);
+            Elements h2Tags = doc.getElementsByTag("h2");
+            List<String> sideEffects = h2Tags.select(":contains(Bijwerkingen)").nextAll().select("p").eachText();
+            List<String> drugDescription = h2Tags.select(":contains(Advies)").nextAll().eachText();
 
         }
 
