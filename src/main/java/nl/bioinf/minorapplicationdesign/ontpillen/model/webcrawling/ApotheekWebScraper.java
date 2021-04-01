e1ea23028d234a4abf259befc6355a1b3e48576c
larissa bouwknegt <l.bouwknegt@st.hanze.nl>
2021-04-01 12:02:18 +0200

    Finished the apotheekwebscraper side effects and added interactions

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ApotheekWebScraper.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ApotheekWebScraper.java
@@ -1,7 +1,12 @@
 package nl.bioinf.minorapplicationdesign.ontpillen.model.webcrawling;
 
 import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.DrugDao;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,18 +20,21 @@ public class ApotheekWebScraper extends AbstractWebScraper {
 
     @Override
     public void parseHtml() {
-        String description = getDescription();
-        String sideEffects = getSideEffects();
-        String stopIndication = getStopIndication();
-        String interactions = getInteractions();
-
-        information.add(description);
-        information.add(sideEffects);
-        information.add(stopIndication);
-        information.add(interactions);
+//        String description = getDescription();
+//        String sideEffects = getSideEffects();
+//        String stopIndication = getStopIndication();
+//        String interactions = getInteractions();
+//
+//        //information.add(description);
+//        information.add(sideEffects);
+//        information.add(stopIndication);
+//        information.add(interactions);
+//        return information;
     }
 
-    private String getInteractions() {
+    private static String getInteractions(Document doc) {
+        Elements interactions = doc.getElementsByAttributeValueContaining("data-print", "andere medicijnen gebruiken").select(".listItemContent_text__otIdg ");
+        System.out.println(interactions.eachText());
         return null;
     }
 
@@ -34,11 +42,50 @@ public class ApotheekWebScraper extends AbstractWebScraper {
         return null;
     }
 
-    private String getSideEffects() {
+    private static String getSideEffects(Document doc) {
+
+        Elements sideEffectsHtmlLocation = doc.getElementsByAttributeValueContaining("data-print", "bijwerkingen");
+        List<String> sideEffectsIntro = sideEffectsHtmlLocation.select(".listItemContent_text__otIdg p, p.listItemContent_text__otIdg").eachText();
+        System.out.println(sideEffectsIntro);
+        Element frequencyAndSideEffect = sideEffectsHtmlLocation.select(".sideEffects_sideEffects__sczbd").get(0);
+        Elements frequency = frequencyAndSideEffect.getElementsByTag("h3");
+        for (Element element: frequency) {
+            Elements sideEffects = element.nextElementSibling().getElementsByClass("sideEffectsItem_button__V-L1C");
+            System.out.println(element.text() + sideEffects.eachText());
+            for (Element sideEffect: sideEffects) {
+                Elements sideEffectDescription = sideEffect.nextElementSibling().select(".sideEffectsItem_content__10s1c");
+                System.out.println(sideEffect.text() + sideEffectDescription.eachText());
+            }
+        }
+
+
+        //TODO Add to datamodel
         return null;
     }
 
-    private String getDescription() {
+    private static String getDescription(Document doc) {
+        Element useIndicationTag = doc.getElementsByAttributeValueContaining("data-print", "waarbij gebruik").select(".listItemContent_text__otIdg").get(0);
+        System.out.println(useIndicationTag.children().eachText());
+        //TODO add to the datamodel
         return null;
+    }
+
+    private static Document getConnection(String medicine) throws IOException {
+        String basicUrl = "https://www.apotheek.nl/medicijnen/";
+        String completeUrl = basicUrl + medicine.toLowerCase();
+        Document doc = Jsoup.connect(completeUrl).get();
+        return doc;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> medicines = List.of("citalopram", "lorazepam", "Temazepam");
+        for (String drug: medicines) {
+            Document doc = getConnection(drug);
+//            getDescription(doc);
+//            getSideEffects(doc);
+            getInteractions(doc);
+
+
+        }
     }
 }
