53f964580d4a8b8677fb7b363e0f33c4a66301f0
Naomi <n.j.hindriks@st.hanze.nl>
2021-04-01 14:25:44 +0200

    Connected front and back end, made spring components

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ImplementationWebcrawler.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ImplementationWebcrawler.java
@@ -1,18 +1,27 @@
 package nl.bioinf.minorapplicationdesign.ontpillen.model.webcrawling;
 
 
+import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.DrugDao;
 import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.InMemoryDrugDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class ImplementationWebcrawler {
-    InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
+    DrugDao drugDao;
+
+    @Autowired
+    public void setDrugDao(DrugDao drugDao) {
+        this.drugDao = drugDao;
+    }
 
     public void runWebcrawlers() throws IOException {
 //        FarmacoWebScraper farmacoWebcrawler = new FarmacoWebScraper(drugDao);
 //        farmacoWebcrawler.getInformation();
 
-        DrugFetcher drugFetcher = new DrugFetcher(informationStorage);
+        DrugFetcher drugFetcher = new DrugFetcher(drugDao);
         drugFetcher.parseHtml();
 
 //        ApotheekWebScraper apotheekWebcrawler = new ApotheekWebScraper(drugDao);
@@ -25,8 +34,9 @@ public class ImplementationWebcrawler {
 //        ggzStandaardenWebcrawler.getInformation();
     }
 
-    public static void main(String[] args) throws IOException {
-        ImplementationWebcrawler test = new ImplementationWebcrawler();
-        test.runWebcrawlers();
-    }
+
+//    public static void main(String[] args) throws IOException {
+//        ImplementationWebcrawler test = new ImplementationWebcrawler();
+//        test.runWebcrawlers();
+//    }
 }
--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/webcontrol/WebController.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/webcontrol/WebController.java
@@ -1,6 +1,9 @@
 package nl.bioinf.minorapplicationdesign.ontpillen.webcontrol;
 
+import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.Drug;
+import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.DrugDao;
 import nl.bioinf.minorapplicationdesign.ontpillen.model.webInteraction.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WebController {
+    DrugDao drugDao;
 
     @GetMapping("/")
     public String showIndex(Model model) {
@@ -19,6 +23,12 @@ public class WebController {
     @GetMapping("/list")
     public String showResult(@ModelAttribute User user, Model model) {
         model.addAttribute("user", user);
+        model.addAttribute("drugDao", this.drugDao);
         return "result";
+    }
+
+    @Autowired
+    public void setDrugDao(DrugDao drugDao) {
+        this.drugDao = drugDao;
     }
 }
