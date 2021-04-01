2d800173e866bd1d873727f95b68273363e38450
JonathanKlimp <j.klimp@st.hanze.nl>
2021-04-01 18:58:52 +0200

    Test now uses Bean drugDao

--- src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/MedicineDAO/InMemoryDrugDaoTest.java
+++ src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/MedicineDAO/InMemoryDrugDaoTest.java
@@ -1,29 +1,37 @@
 package nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ComponentScan(basePackageClasses = {DrugDao.class})
 class InMemoryDrugDaoTest {
+
+    @Autowired
+    DrugDao drugDao;
 
     @Test
     void addDrug_addNewSubstance_returnNewDrugSubstance() {
         DrugSubstance newDrugSubstance = new DrugSubstance("Citalopram");
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
-        informationStorage.addDrug(newDrugSubstance);
-        assertEquals(newDrugSubstance, informationStorage.getDrugByName("Citalopram"));
+        drugDao.addDrug(newDrugSubstance);
+        assertEquals(newDrugSubstance, drugDao.getDrugByName("Citalopram"));
     }
 
     @Test
     void addDrug_addDuplicateDrugSubstance_throwIllegalArgumentException() {
         DrugSubstance newDrugSubstance1 = new DrugSubstance("Citalopram");
         DrugSubstance newDrugSubstance2 = new DrugSubstance("Citalopram");
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
         try {
-            informationStorage.addDrug(newDrugSubstance1);
-            informationStorage.addDrug(newDrugSubstance2);
+            drugDao.addDrug(newDrugSubstance1);
+            drugDao.addDrug(newDrugSubstance2);
             fail();
         }
         catch (IllegalArgumentException exception){
@@ -34,19 +42,17 @@ class InMemoryDrugDaoTest {
     @Test
     void addDrug_addNewDrugGroup_returnNewDrugGroup(){
         DrugGroup newDrugGroup = new DrugGroup("Serotonineheropnameremmers, selectief");
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
-        informationStorage.addDrug(newDrugGroup);
-        assertEquals(newDrugGroup, informationStorage.getDrugByName("Serotonineheropnameremmers, selectief"));
+        drugDao.addDrug(newDrugGroup);
+        assertEquals(newDrugGroup, drugDao.getDrugByName("Serotonineheropnameremmers, selectief"));
     }
 
     @Test
     void addDrug_addDuplicateDrugGroup_throwIllegalArgumentException() {
         DrugGroup newDrugGroup1 = new DrugGroup("Serotonineheropnameremmers, selectief");
         DrugGroup newDrugGroup2 = new DrugGroup("Serotonineheropnameremmers, selectief");
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
         try {
-            informationStorage.addDrug(newDrugGroup1);
-            informationStorage.addDrug(newDrugGroup2);
+            drugDao.addDrug(newDrugGroup1);
+            drugDao.addDrug(newDrugGroup2);
             fail();
         }
         catch (IllegalArgumentException exception){
@@ -57,16 +63,14 @@ class InMemoryDrugDaoTest {
     @Test
     void getDrugByName_DrugInStorage_returnDrug() {
         DrugGroup newDrugGroup = new DrugGroup("Citalopram");
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
-        informationStorage.addDrug(newDrugGroup);
-        assertEquals(newDrugGroup, informationStorage.getDrugByName("Citalopram"));
+        drugDao.addDrug(newDrugGroup);
+        assertEquals(newDrugGroup, drugDao.getDrugByName("Citalopram"));
     }
 
     @Test
     void getDrugByName_DrugNotInStorage_throwIllegalArgumentException() {
-        InMemoryDrugDao informationStorage = InMemoryDrugDao.getInstance();
         try {
-            informationStorage.getDrugByName("Nicotine");
+            drugDao.getDrugByName("Nicotine");
         }
         catch(IllegalArgumentException exception){
             assertEquals("Nicotine does not exist.", exception.getMessage());
