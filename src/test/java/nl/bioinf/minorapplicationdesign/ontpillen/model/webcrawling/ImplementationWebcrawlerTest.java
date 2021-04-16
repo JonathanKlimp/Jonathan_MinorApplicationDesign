692f04bb6ece6ef8249c35430fffc4e5dc2120e7
JonathanKlimp <j.klimp@st.hanze.nl>
2021-04-16 14:59:55 +0200

    Wrote tests for parsehtml, that checks if the correct drugs/drugGroups/drugSubstances are returned

--- src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ImplementationWebcrawlerTest.java
+++ src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/webcrawling/ImplementationWebcrawlerTest.java
@@ -1,6 +1,6 @@
 package nl.bioinf.minorapplicationdesign.ontpillen.model.webcrawling;
 
-import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.DrugDao;
+import nl.bioinf.minorapplicationdesign.ontpillen.model.MedicineDAO.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,13 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class ImplementationWebcrawlerTest {
@@ -22,15 +18,43 @@ class ImplementationWebcrawlerTest {
     @Autowired
     DrugDao drugDao;
 
-
     @Test
-    void testRunWebcrawlers() throws IOException {
+    void runWebcrawlers_parseHtml_returnsAllDrugs() throws IOException {
         String[] drugNamesArray = {"alprazolam", "oxazepam", "nalmefeen", "zuclopentixol", "fenelzine", "mianserine", "antipsychotica, atypische", "nitrazepam", "amisulpride", "clobazam", "dapoxetine", "midazolam", "chloorprotixeen", "psychostimulantia", "buprenorfine/naloxon", "benzodiazepine agonisten", "dosulepine", "sertraline", "methylfenidaat", "nortriptyline", "middelen bij verslavingsziekten", "prazepam", "clorazepinezuur", "modafinil", "middelen bij alcoholverslaving", "broomperidol", "zolpidem", "melatonine", "tranylcypromine", "dexamfetamine", "risperidon", "diazepam", "coffeïne", "antipsychotica", "antipsychotica, klassieke", "brotizolam", "paliperidon", "varenicline", "venlafaxine", "fluoxetine", "antidepressiva", "periciazine", "moclobemide", "serotonineheropnameremmers, selectief", "sertindol", "amitriptyline", "imipramine", "fluvoxamine", "antidepressiva, overige", "paroxetine", "duloxetine", "cariprazine", "psycholeptica", "flunitrazepam", "brexpiprazol", "quetiapine", "pimozide", "flurazepam", "lithium", "buprenorfine (bij verslaving)", "serotonineheropnameremmers, niet-selectief", "aripiprazol", "citalopram", "clomipramine", "temazepam", "nicotine", "loprazolam", "lorazepam", "middelen bij nicotineverslaving", "psychofarmaca, overige", "fluspirileen", "lithiumzouten", "melatonine agonisten", "lormetazepam", "flupentixol", "clozapine", "buspiron", "bupropion", "MAO-remmers, niet-selectief", "methadon", "vortioxetine", "disulfiram", "atomoxetine", "lisdexamfetamine", "zopiclon", "acamprosaat", "olanzapine", "haloperidol", "bromazepam", "penfluridol", "trazodon", "pipamperon", "escitalopram", "MAO-A-remmers", "middelen bij opioïdverslaving", "slaapmiddelen", "sulpiride", "mirtazapine", "tricyclische antidepressiva", "psychostimulantia, overige", "maprotiline", "naltrexon", "agomelatine", "lurasidon", "amfetaminen", "esketamine (nasaal)", "tetracyclische antidepressiva"};
-        List drugNames = new ArrayList<>(Arrays.asList(drugNamesArray));
+        List<String> drugNames = new ArrayList<>(Arrays.asList(drugNamesArray));
 
         DrugFetcher drugFetcher = new DrugFetcher(drugDao);
         drugFetcher.parseHtml();
 
         assertEquals(drugNames, drugDao.getAllDrugNames());
+    }
+
+    @Test
+    void runWebcrawlers_parseHtml_returnsCorrectDrugSubstances() throws IOException {
+        DrugFetcher drugFetcher = new DrugFetcher(drugDao);
+        drugFetcher.parseHtml();
+        String[] drugSubstances = {"alprazolam", "oxazepam", "nalmefeen", "quetiapine", "pimozide", "flurazepam", "zuclopentixol", "fenelzine", "lithium", "mianserine", "buprenorfine (bij verslaving)", "nitrazepam", "amisulpride", "clobazam", "dapoxetine", "midazolam", "chloorprotixeen", "buprenorfine/naloxon", "aripiprazol", "citalopram", "dosulepine", "sertraline", "methylfenidaat", "nortriptyline", "clomipramine", "temazepam", "nicotine", "loprazolam", "prazepam", "clorazepinezuur", "modafinil", "lorazepam", "broomperidol", "fluspirileen", "lormetazepam", "zolpidem", "flupentixol", "melatonine", "tranylcypromine", "clozapine", "dexamfetamine", "buspiron", "bupropion", "methadon", "risperidon", "diazepam", "vortioxetine", "coffeïne", "disulfiram", "brotizolam", "atomoxetine", "lisdexamfetamine", "zopiclon", "paliperidon", "acamprosaat", "varenicline", "olanzapine", "haloperidol", "venlafaxine", "fluoxetine", "bromazepam", "penfluridol", "trazodon", "pipamperon", "periciazine", "escitalopram", "moclobemide", "sulpiride", "sertindol", "amitriptyline", "mirtazapine", "imipramine", "maprotiline", "naltrexon", "fluvoxamine", "agomelatine", "lurasidon", "paroxetine", "esketamine (nasaal)", "duloxetine", "cariprazine", "flunitrazepam", "brexpiprazol"};
+        List<String> expectedDrugSubstances = new ArrayList<>(Arrays.asList(drugSubstances));
+        List<String> actualDrugSubstances = new ArrayList<>();
+
+        for(Drug drugSubstance : drugDao.getDrugSubstances()){
+            actualDrugSubstances.add(drugSubstance.getName());
+        }
+        assertEquals(expectedDrugSubstances, actualDrugSubstances);
+    }
+
+    @Test
+    void runWebcrawlers_parseHtml_returnCorrectDrugGroups() throws IOException {
+        DrugFetcher drugFetcher = new DrugFetcher(drugDao);
+        drugFetcher.parseHtml();
+
+        String[] drugGroups = {"middelen bij verslavingsziekten", "antidepressiva", "psychostimulantia", "slaapmiddelen", "psychofarmaca, overige", "psycholeptica"};
+        List<String> expectedDrugGroups = new ArrayList<>(Arrays.asList(drugGroups));
+        List<String> actualDrugGroups = new ArrayList<>();
+
+        for(Drug drugGroup: drugDao.getMainDrugGroups()){
+            actualDrugGroups.add(drugGroup.getName());
+        }
+        assertEquals(expectedDrugGroups, actualDrugGroups);
     }
 }
