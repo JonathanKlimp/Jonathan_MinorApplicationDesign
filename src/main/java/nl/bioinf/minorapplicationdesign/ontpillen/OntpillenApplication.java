e8af086a57ba8d092ce7a63ac052fab76692ce5b
JonathanKlimp <j.klimp@st.hanze.nl>
2021-04-26 14:38:08 +0200

    Added log messages to OntpillenApplication

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/OntpillenApplication.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/OntpillenApplication.java
@@ -1,11 +1,15 @@
 package nl.bioinf.minorapplicationdesign.ontpillen;
 
 import nl.bioinf.minorapplicationdesign.ontpillen.model.web_scraping.WebScrapeExecutor;
+import org.apache.juli.logging.Log;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -13,8 +17,10 @@ import java.io.IOException;
  */
 @SpringBootApplication
 public class OntpillenApplication {
+    private static Logger LOGGER = LoggerFactory.getLogger(OntpillenApplication.class);
 
     public static void main(String[] args) throws IOException {
+        LOGGER.info("Starting the application");
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MyApplicationContextConfiguration.class);
 
         WebScrapeExecutor myWebScraper = ctx.getBean(WebScrapeExecutor.class);
