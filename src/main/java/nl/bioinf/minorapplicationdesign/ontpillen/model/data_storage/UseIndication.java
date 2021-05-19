ccb94dcec6748fcb71af210d78d7be5ffd974ef6
JonathanKlimp <j.klimp@st.hanze.nl>
2021-05-19 12:50:54 +0200

    Copied UseIndication  from the repo of larissa to implement it in the project

--- src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/data_storage/UseIndication.java
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/data_storage/UseIndication.java
@@ -4,6 +4,13 @@ package nl.bioinf.minorapplicationdesign.ontpillen.model.data_storage;
  *
  * @author Larissa Bouwknegt
  */
+
 public class UseIndication {
-    String name;
+    private String name;
+
+    public void setName(String useIndication) {
+        this.name = useIndication;
+    }
+
+    public String getName() {return name;}
 }
