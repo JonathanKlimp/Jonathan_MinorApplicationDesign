ac578edb6fad8be2a6778d4ae9de6226b124890e
JonathanKlimp <j.klimp@st.hanze.nl>
2021-05-19 12:50:36 +0200

    Copied and modified IndicationScraper from the repo of larissa to implement it in the project

---
+++ src/main/java/nl/bioinf/minorapplicationdesign/ontpillen/model/web_scraping/IndicationScraper.java
@@ -0,0 +1,71 @@
+package nl.bioinf.minorapplicationdesign.ontpillen.model.web_scraping;
+
+import nl.bioinf.minorapplicationdesign.ontpillen.model.data_storage.Drug;
+import nl.bioinf.minorapplicationdesign.ontpillen.model.data_storage.DrugDao;
+import nl.bioinf.minorapplicationdesign.ontpillen.model.data_storage.DrugSubstance;
+import nl.bioinf.minorapplicationdesign.ontpillen.model.data_storage.UseIndication;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Larissa Bouwknegt en Jonathan Klimp
+ */
+
+@Component
+@PropertySource("classpath:application.properties")
+public class IndicationScraper implements AbstractWebScraper{
+    private DrugDao drugDao;
+    private String url;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DrugFetcher.class);
+
+    private IndicationScraper(@Value("${farmaco.basic.site}") String url) {
+        this.url = url;
+    }
+
+
+    @Autowired
+    public void setDrugDao(DrugDao drugDao) {
+        this.drugDao = drugDao;
+    }
+
+    @Override
+    public void parseHtml() throws IOException {
+        LOGGER.info("Running parseHtml");
+        SSLHelper.bypassSSL();
+        Document doc = Jsoup.connect(this.url).get();
+        Elements indicationHtmlElements = doc.getElementsByClass("pat-rich group-1").select("h2");
+        for (Element indication: indicationHtmlElements) {
+            String useIndication = indication.text();
+            List<String> parsedDrugs = indication.nextElementSiblings().select("li").eachText();
+            checkDrugDao(parsedDrugs, indication.text());
+            LOGGER.debug("The indication is "+ useIndication + " the drugs are " + parsedDrugs);
+        }
+    }
+
+    private void checkDrugDao(List<String> parsedDrugs, String indication){
+        for (String drug: parsedDrugs) {
+            if (drugDao.drugInDrugDao(drug)) {
+                Drug currentDrug = drugDao.getDrugByName(drug);
+                DrugSubstance drugSubstance = (DrugSubstance) currentDrug;
+                UseIndication newUseIndication = new UseIndication();
+                newUseIndication.setName(indication);
+                drugSubstance.addUseIndication(newUseIndication);
+            }
+            else {
+                LOGGER.info(drug + " is not present in the dao");
+            }
+        }
+    }
+}
