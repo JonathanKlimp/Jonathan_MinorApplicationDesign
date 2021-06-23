ef6df9f2350403be70fa7e080724669399d7b149
JonathanKlimp <j.klimp@st.hanze.nl>
2021-06-23 14:35:48 +0200

    split multiple test methods since there were multiple assertions

--- src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/data_storage/content/ContentNodeTest.java
+++ src/test/java/nl/bioinf/minorapplicationdesign/ontpillen/model/data_storage/content/ContentNodeTest.java
@@ -16,8 +16,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ContentNodeTest {
 
-    @Test //TODO split test there are to many asserts in one method
-    public void setContent() {
+    @Test
+    public void setContent_contentNotNull() {
 //        Create ContentNode object
         ContentNode contentNode = new ContentNode();
 //        Create new content to be added to ContentNode object
@@ -31,22 +31,58 @@ class ContentNodeTest {
         contentNode.setContent(contentToAddInList);
 
 //        Test if content has been added
-        assertTrue(contentNode.getContent() != null);
+        assertNotNull(contentNode.getContent());
+
+    }
+
+    @Test
+    public void setContent_CorrectContentType() {
+//        Create ContentNode object
+        ContentNode contentNode = new ContentNode();
+//        Create new content to be added to ContentNode object
+        ContentLeaf contentToAdd = new ContentLeaf();
+        contentToAdd.setContent(new ArrayList<>(Arrays.asList("test1", "test2", "test3")));
+        contentToAdd.setContentType("PARAGRAPH");
+        List<Content> contentToAddInList = new ArrayList<>() {{
+            add(contentToAdd);
+        }};
+//        set the created content to the ContentNode object
+        contentNode.setContent(contentToAddInList);
 
 //        Test if the content that has been added is the correct content
         for (Content content: contentNode.getContent()) {
             ContentLeaf contentAsLeaf = (ContentLeaf) content;
             assertEquals("PARAGRAPH",  contentAsLeaf.contentType.name());
+        }
+    }
+
+    @Test
+    public void setContent_CorrectContentValue() {
+//        Create ContentNode object
+        ContentNode contentNode = new ContentNode();
+//        Create new content to be added to ContentNode object
+        ContentLeaf contentToAdd = new ContentLeaf();
+        contentToAdd.setContent(new ArrayList<>(Arrays.asList("test1", "test2", "test3")));
+        contentToAdd.setContentType("PARAGRAPH");
+        List<Content> contentToAddInList = new ArrayList<>() {{
+            add(contentToAdd);
+        }};
+//        set the created content to the ContentNode object
+        contentNode.setContent(contentToAddInList);
+
+//        Test if the content that has been added is the correct content
+        for (Content content: contentNode.getContent()) {
+            ContentLeaf contentAsLeaf = (ContentLeaf) content;
             int i = 0;
             for (String contentOfLeaf : contentAsLeaf.getContent()) {
                 i += 1;
                 assertEquals("test" + Integer.toString(i), contentOfLeaf);
             }
-        };
+        }
     }
 
-    @Test //TODO split test there are to many asserts in one method
-    void addContent_addSingleContent() {
+    @Test
+    void addContent_addSingleContent_contentAdded() {
 //        Create ContentNode object
         ContentNode contentNode = new ContentNode();
 //        Create new content to be added to ContentNode object
@@ -58,10 +94,36 @@ class ContentNodeTest {
 
 //        Test if content has been added
         assertEquals(1, contentNode.getContent().size());
+    }
+
+    @Test
+    void addContent_addSingleContent_correctContentType() {
+//        Create ContentNode object
+        ContentNode contentNode = new ContentNode();
+//        Create new content to be added to ContentNode object
+        ContentLeaf contentToAdd = new ContentLeaf();
+        contentToAdd.setContent(new ArrayList<>(Arrays.asList("test1", "test2", "test3")));
+        contentToAdd.setContentType("PARAGRAPH");
+//        Add content to ContentNode object
+        contentNode.addContent(contentToAdd);
 
 //        Test if the content that has been added is the correct content
         ContentLeaf contentAsLeaf = (ContentLeaf) contentNode.getContent().get(0);
         assertEquals("PARAGRAPH",  contentAsLeaf.contentType.name());
+    }
+
+    @Test
+    void addContent_addSingleContent_correctContentValue() {
+//        Create ContentNode object
+        ContentNode contentNode = new ContentNode();
+//        Create new content to be added to ContentNode object
+        ContentLeaf contentToAdd = new ContentLeaf();
+        contentToAdd.setContent(new ArrayList<>(Arrays.asList("test1", "test2", "test3")));
+        contentToAdd.setContentType("PARAGRAPH");
+//        Add content to ContentNode object
+        contentNode.addContent(contentToAdd);
+
+        ContentLeaf contentAsLeaf = (ContentLeaf) contentNode.getContent().get(0);
         int i = 0;
         for (String contentOfLeaf : contentAsLeaf.getContent()) {
             i += 1;
@@ -69,8 +131,11 @@ class ContentNodeTest {
         }
     }
 
+    /**
+     * Test checks if adding multiple content doesnt overwrite the older ones.
+     */
     @Test //TODO split test there are to many asserts in one method
-    void addContent_addMultipleContents() {
+    void addContent_addMultipleContents_CorrectContentTypes() {
 //        Create ContentNode object
         ContentNode contentNode = new ContentNode();
 //        Create new content to be added to ContentNode object
@@ -87,29 +152,31 @@ class ContentNodeTest {
         contentNode.addContent(contentToAdd1);
         contentNode.addContent(contentToAdd2);
 
-//        Test if content has been added
-        assertEquals(2, contentNode.getContent().size());
-
-//        Test if the content that has been added is the correct content
+//        Test if the content that has been added is the correct content and is not overwritten
         ContentLeaf contentAsLeaf = (ContentLeaf) contentNode.getContent().get(0);
         assertEquals("PARAGRAPH",  contentAsLeaf.contentType.name());
-        int i = 0;
-        for (String contentOfLeaf : contentAsLeaf.getContent()) {
-            i += 1;
-            assertEquals("test" + Integer.toString(i), contentOfLeaf);
-        }
-
-        contentAsLeaf = (ContentLeaf) contentNode.getContent().get(1);
-        assertEquals("LIST",  contentAsLeaf.contentType.name());
-        i = 0;
-        for (String contentOfLeaf : contentAsLeaf.getContent()) {
-            i += 1;
-            assertEquals("test" + Integer.toString(i), contentOfLeaf);
-        }
     }
 
     @Test
-    void getContent_tryingTOModifyList() {
+    void getContent_tryingToRemoveFromList() {
+//        Create ContentNode object
+        ContentNode contentNode = new ContentNode();
+//        Create new content to be added to ContentNode object
+        ContentLeaf contentToAdd = new ContentLeaf();
+        contentToAdd.setContent(new ArrayList<>(Arrays.asList("test1", "test2", "test3")));
+        contentToAdd.setContentType("PARAGRAPH");
+
+//        Get the content of the ContentNode object
+        List<Content> content = contentNode.getContent();
+
+//        Test if an UnsupportedOperationException is thrown when removing an item of the list
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+            content.remove(contentToAdd);
+        });
+    }
+
+    @Test
+    void getContent_tryingToAddToList() {
 //        Create ContentNode object
         ContentNode contentNode = new ContentNode();
 //        Create new content to be added to ContentNode object
@@ -123,11 +190,6 @@ class ContentNodeTest {
 //        Test if an UnsupportedOperationException is thrown when adding to the list
         Assertions.assertThrows(UnsupportedOperationException.class, () -> {
             content.add(new ContentLeaf());
-        });
-
-//        Test if an UnsupportedOperationException is thrown when removing an item of the list
-        Assertions.assertThrows(UnsupportedOperationException.class, () -> {
-            content.remove(contentToAdd);
         });
     }
 }
